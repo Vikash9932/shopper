bc49c60546feeb3e986929fa8672bfce296d36d5
Vikash <vikash9932@gmail.com>
2021-11-27 13:08:43 +0530

    Coloring console

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,9 +5,9 @@ const connectDB = async () => {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
       useUnifiedTopology: true,
     });
-    console.log(`MongoDB connected: ${conn.connection.host}`);
+    console.log(`MongoDB connected: ${conn.connection.host}`.cyan.underline);
   } catch (error) {
-    console.error(`Error: ${error.message}`);
+    console.error(`Error: ${error.message}`.red.bold.underline);
     process.exit(1);
   }
 };
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import colors from 'colors';
 import connectDB from './config/db.js';
 import products from './data/products.js';
 
@@ -24,5 +25,8 @@ app.get('/api/products/:id', (req, res) => {
 const PORT = process.env.PORT || 5000;
 app.listen(
   PORT,
-  console.log(`server runnning in ${process.env.NODE_ENV} mode at port ${PORT}`)
+  console.log(
+    `server runnning in ${process.env.NODE_ENV} mode at port ${PORT}`.yellow
+      .bold
+  )
 );
