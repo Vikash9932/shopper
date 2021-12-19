bd9241f4daf21fdaf2f63938b9dc13fe8d106482
Vikash <vikash9932@gmail.com>
2021-12-19 12:19:46 +0530

    Prepare to deploy

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
 import colors from 'colors';
@@ -13,14 +14,24 @@ const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Api is running');
-});
-
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
-app.use(notFound);
 
+const __dirname = path.resolve();
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  );
+} else {
+  app.get('/', (req, res) => {
+    res.send('Api is running');
+  });
+}
+
+app.use(notFound);
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
